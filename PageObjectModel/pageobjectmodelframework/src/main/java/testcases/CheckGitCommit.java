b69c0f94a85605e3073489aeb4ec06e3d4d1f434
Kiran Reddy <kiranreddy365@gmail.com>
2019-08-17 09:28:19 +0530

    added to git

---
+++ PageObjectModel/pageobjectmodelframework/src/main/java/testcases/CheckGitCommit.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class CheckGitCommit {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("In Test1.");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("In Test2.");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("In Test3.");
+	}
+
+}
